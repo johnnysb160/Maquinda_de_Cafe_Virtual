5dd3ddc7c54b8b19c434c787c603ef585b875e76
Johnny Barros <johnnysb@gmail.com>
2020-10-27 21:10:19 -0300

    Criação do sistema em WinForm

---
+++ Maquina_de_Cafe_Virtual/Maquina_de_Cafe_Virtual/TelaPrincipal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maquina_de_Cafe_Virtual
+{
+    public partial class TelaPrincipal : Form
+    {
+        public TelaPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
